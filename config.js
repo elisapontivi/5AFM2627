17ddd961fef1f7c9800fcf9817804098c5e6f002
elisapontivi <77245239+elisapontivi@users.noreply.github.com>
2026-09-20 21:38:15 +0200

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-// Public key only. Authentication and permissions are enforced by the server.
-window.CLASSE_CONFIG=Object.freeze({supabaseUrl:"https://qjrqoznrpkawhlttfmyk.supabase.co",publishableKey:'sb_publishable_RlgDFVGmN4eXvu3JuiwkcQ_OuozWlpA',studentDomain:'classe.invalid'});
